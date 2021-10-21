b5168897df864521f5898d9afde88285af765cbb
Vidhiya <vidhiyae@dckap.com>
2021-10-21 15:54:12 +0530

    interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
+    var p, n,r;
     p = document.getElementById("principal").value;
-    
+    n = document.getElementById("years").value;
+    r = document.getElementById("rate").value;
+    let interest = parseInt((p * n * r) / 100)
+    document.getElementById ('num').innerHTML =`<p>If you deposit <span id="hl-span">${p}</span> at an interest rate of <span id="hl-span">${r}</span> you will receive an amount of <span id="hl-span">${interest}</span></p>`;
 }
-        
+(function() {
+    var number=50;
+    var optionList = "";
+    for (var x=1; x<=number; x++) {
+    optionList += "<option>"+x+"</option>";
+    }
+    console.log("optionList", optionList)
+    var select = document.getElementById('years')
+    select.innerHTML = optionList;
+ 
+ })();
+
